619a98eb00e963e3d2bb390d619aa9c7dd3dbfd5
Aiden Madaffri <contact@aidenmadaffri.com>
2020-06-28 07:40:30 -0500

    Form checks choices in a method

--- recs/forms.py
+++ recs/forms.py
@@ -4,10 +4,13 @@ from django.core import validators
 from .models import Genre
 
 class SubmitForm(forms.Form):
-    choices = list()
-    for genre in Genre.objects.all():
-        choices.append((genre.pk, genre.name))
+    def getChoices():
+        choices = list()
+        for genre in Genre.objects.all():
+            choices.append((genre.pk, genre.name))
+        return choices
 
+    choices = getChoices()
     url = forms.CharField(label="Game's Steam URL:", validators=[validators.RegexValidator(regex='^(https://)?store\.steampowered\.com/app/(\d+).*$', message='Please provide a valid store page link, e.x. https://store.steampowered.com/app/570/Dota_2/')])
     submitter = forms.CharField(max_length=50, required=False)
     genres = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=choices)
